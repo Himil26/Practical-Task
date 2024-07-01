671c0b26c181805fc6f7873cec8b1bdeed1fd5c5
Himil patel <patelhimil467@gmail.com>
2024-07-01 12:55:39 +0530

    delete two files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import ProductDetails from './components/ProductDetails';
 import ProductList from './components/ProductList';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {CartProvider} from './Context/CartContext';
-import Cart from './components/Cart';
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Routes>
           <Route path="/" element={<ProductList />} />
           <Route path="/product/:id" element={<ProductDetails />} />
-          <Route path="/cart" element={<Cart />} />
         </Routes>
       </BrowserRouter>
     </CartProvider>
